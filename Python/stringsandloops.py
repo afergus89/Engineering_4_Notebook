839c8edb9d3653344dc21452eabe83164265c812
Molly and Alex <afergus89@charlottesvilleschools.org>
2018-09-10 12:13:56 -0400

    adding strings and loops program

---
+++ Python/stringsandloops.py
@@ -0,0 +1,12 @@
+# Strings and loops
+# Molly and Alex
+
+x = input("Enter a simple sentence:")
+# Hello world
+
+for a in x:
+    if a == (" "):
+        print ("-")
+    else:
+        print(a)
+    
