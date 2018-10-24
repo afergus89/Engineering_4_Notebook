2b260e0e59a1b65e8e13dc856587c63b41415706
Molly and Alex <afergus89@charlottesvilleschools.org>
2018-10-24 12:09:17 -0400

    adding changes to app.py

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import RPi.GPIO as GPIO
+import os
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -11,20 +12,25 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
 	if request.method == "POST":
-		GPIO.output(17,GPIO.HIGH)
+		print request.form
 		msg = request.form.get("submitBtn")
+		if msg == "blue led":
+			if GPIO.input(17):
+				GPIO.output(17,GPIO.LOW)
+			else:
+				GPIO.output(17,GPIO.HIGH)
+		if msg == "red led":
+			if GPIO.input(18):
+				GPIO.output(18,GPIO.LOW)
+			else:
+				GPIO.output(18,GPIO.HIGH)
 	else:
-		GPIO.output(17,GPIO.LOW)
 		msg = "No click yet."
 	return render_template("index.html", msg=msg)
 
-	if request.method == "POST":
-		GPIO.output(18,GPIO.HIGH)
-		msg = request.form.get("submitBTN")
-	else:
-		GPIO.output(18,GPIO.LOW)
-		msg = "No click yet."
-	return render_template("index.html", msg=msg)
+@app.route('/pups/')
+def pic():
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'twopups.jpg',mimetype='image/jpeg')
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
